6960a0538527e2eee79f915b9b0d7672ec6afa30
anshuldhargave <dhargaveanshul@gmail.com>
2024-04-23 17:28:47 +0100

    Simple Blackjack game
    Initial Commit

---
+++ index.js
@@ -0,0 +1,73 @@
+// let player = {
+//     name:"Anshul",
+//     chips: 200
+// }
+
+let cards = [];
+let sum = 0;
+let hasBlackjack = false;
+let isAlive = false;
+let message = "";
+
+// let playerEl = document.getElementById("player-el")
+// playerEl.textContent = player.name + ": $" + player.chips
+
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
+}
+
+function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
+  renderGame();
+}
+
+function renderGame() {
+  let cardsEl = document.getElementById("cards-el");
+  cardsEl.textContent = "Cards: ";
+
+  for (i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+
+  let sumEl = document.getElementById("sum-el");
+  sumEl.textContent = "Sum: " + sum;
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+    isAlive = true;
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    hasBlackjack = true;
+  } else {
+    message = "You're out of the game?";
+    isAlive = false;
+  }
+  let messageEl = document.getElementById("message-el");
+  messageEl.textContent = message;
+}
+
+function newCard() {
+  if (isAlive === true && hasBlackjack === false) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    renderGame();
+  }
+}
+
+// function Winning() {
+//     if (hasBlackjack === true) {
+//       return (player.chips = player.chips + 100);
+//     }
+//   }
+
---
+++ practice.js
@@ -0,0 +1,94 @@
+// let featuredPosts = [
+//     "Check out my Netflix clone",
+//     "Here's the code for my project",
+//     "I've just relaunched my portfolio"
+// ]
+
+
+// for (let i = 10; i < 101; i+= 10) {
+//     console.log(i);
+// }
+
+
+// for (let i = 0; i < 5; i+= 1){
+//     log()
+// }
+
+// let cards = [7,3,9]
+
+// for (i = 0; i < cards.length; i++){
+//     console.log(cards[i])
+// }
+
+// let sentence = ["Hello ", "my ", "name ", "is ", "Anshul"]
+// let greetingEl = document.getElementById("greeting-el");
+
+// for (i = 0; i < sentence.length; i++) {
+//     greetingEl.textContent = sentence[i];
+// }
+
+
+//   let randomNumber = Math.random() * 6
+//     console.log(randomNumber)
+
+
+// let flooredNumber = Math.floor(5.755435435435432)
+//     console.log(flooredNumber);
+
+
+
+
+
+// function rollDice() {
+//     let randomNumber = Math.floor(Math.random() * 6) + 1
+//     return randomNumber;
+// }
+// console.log(rollDice());
+
+// let hasSolvedChallenge = false;
+// let hasHintsLeft = false;
+
+// if (hasSolvedChallenge === false && hasHintsLeft === false){
+//     showSolution();
+// }
+
+// function showSolution(){
+//     console.log("Showing the solution...")
+// }
+
+// let castle = {
+//     title: "Live live a king in my castle",
+//     isSuprHost: true,
+//     price: $190
+// }
+
+
+
+// let person = {
+//     name: "Anshul",
+//     age: 29,
+//     country: "India"
+// }
+
+// function logData() {
+//     console.log(person.name + " is " + person.age + " years old and lives in " + person.country); 
+// }
+
+// logData()
+
+
+// let largeCountries = ["China", "india", "Ireland", "Japan", "Australia"]
+
+// for (let i = 0; i < largeCountries.length; i++){
+//     console.log("- " + largeCountries[i]);
+// }
+ 
+
+// let hands = ["rock", "paper", "scissors"]
+
+// function getHand(){
+//     let randomIndex = Math.floor(Math.random() * 3)
+//     return hands[randomIndex]
+// }
+
+// console.log(getHand())
